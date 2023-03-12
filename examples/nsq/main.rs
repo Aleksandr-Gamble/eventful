e6a19887e885f20e8acd8fbb14175d2607040e25
Aleksandr-Gamble <aleksandr.gamble@gmail.com>
2023-03-11 23:15:01 -0500

    trait GenConsumer

---
+++ examples/nsq/main.rs
@@ -0,0 +1,77 @@
+use std::{time::Duration, collections::HashSet};
+use tokio::{time::sleep};
+use tokio_nsq::{NSQTopic, NSQChannel, NSQConsumerConfig, NSQConsumerConfigSources, NSQConsumerLookupConfig};
+use rand::{Rng, distributions::{Alphanumeric, DistString}};
+use serde::{Serialize, Deserialize};
+use eventful::{err::GenericError, nsq::{DaemonNSQ, EventNSQ, GenConsumer}};
+
+
+#[derive(Serialize, Deserialize)]
+struct UserClickedSomething {
+    pub user_id: i32,
+    pub clicked_on: String,
+}
+
+impl EventNSQ for UserClickedSomething {
+    fn topic() -> &'static str {
+        "click"
+    }
+}
+
+pub struct ClickProcessor{}
+
+
+impl eventful::nsq::GenConsumer<UserClickedSomething> for ClickProcessor {
+    fn channel(&self) -> String {
+        format!("some_channel")
+    }
+}
+
+impl ClickProcessor {
+    async fn run(&self) -> Result<(), GenericError> {
+        let mut consumer = self.consumer();
+        loop {
+            let message = consumer.consume_filtered().await.unwrap();
+            let event = self.deserialize_event(&message)?;
+            println!("    CONSUME:  user_id={} clicked_on='{}'", &event.user_id, &event.clicked_on);
+            message.finish().await;
+        }
+        Ok(())
+    }
+}
+
+
+async fn simulate_clicks() -> Result<(), GenericError> {
+    let nsqd = DaemonNSQ::new("http://127.0.0.1:4151");
+    loop {
+        let millis: u64 = rand::thread_rng().gen_range(10..750);
+        let user_id = rand::thread_rng().gen_range(0..1000);
+        let clicked_on: String = Alphanumeric.sample_string(&mut rand::thread_rng(), 16);
+        sleep(Duration::from_millis(millis)).await;
+        let event = UserClickedSomething{user_id, clicked_on};
+        println!("PRODUCE: user_id={} clicked_on='{}'", &event.user_id, &event.clicked_on);
+        let _x = event.publish_to(&nsqd.host).await?;
+
+    }
+    Ok(())
+}
+
+async fn consume_events() -> Result<(), GenericError> {
+    let cp = ClickProcessor{};
+    cp.run().await
+}
+
+
+#[tokio::main]
+async fn main() -> Result<(), GenericError> {
+    
+    tokio::spawn(async move {
+        let _ = simulate_clicks().await;
+    });
+
+    // let events accumulate in NSQ for a few seconds to illustrate the decoupled nature of the producer and the consumer
+    sleep(Duration::from_millis(2000u64)).await;
+    let _ = consume_events().await?;
+
+    Ok(())
+}
--- src/nsq.rs
+++ src/nsq.rs
@@ -1,21 +1,26 @@
-use std::env;
+
 use async_trait::async_trait;
 use serde::{Serialize, de::DeserializeOwned};
+use tokio_nsq;
 use hyperactive;
 use crate::err::GenericError;
 
 
-
-pub struct Daemon {
-    host: String,
+pub struct EventMessage<T> {
+    pub message: tokio_nsq::NSQMessage,
+    pub event: T
+}
+pub struct DaemonNSQ {
+    pub host: String,
 }
 
 
-impl Daemon {
+impl DaemonNSQ {
     /// Instantiate a new Daemon client from a url, typically something like http://127.0.0.1:4151
     pub fn new(host: &str) -> Self {
-        Daemon{host: host.to_string()}
+        DaemonNSQ{host: host.to_string()}
     }
+
 }
 
 
@@ -24,19 +29,57 @@ impl Daemon {
 /// All you have to do is define a topic to publish the message to NSQ.
 /// Then you can call .publish(hsqd_host) asynchronously to publish the event.
 #[async_trait]
-pub trait Event: Serialize + DeserializeOwned {
-    fn topic(&self) -> &'static str;
-    async fn publish_to(&self, daemon: &Daemon) -> Result<(), GenericError>  {
-        let topic = self.topic();
-        let _x = post_msg(daemon, &topic, &self).await?;
+pub trait EventNSQ: Serialize + DeserializeOwned {
+    fn topic() -> &'static str;
+    async fn publish_to(&self, host: &str) -> Result<(), GenericError>  {
+        let topic =  <Self as EventNSQ>::topic();
+        let _x = post_json(host, &topic, &self).await?;
         Ok(())
     }
 }
 
 
+pub trait GenConsumer<T: EventNSQ> {
+    /// This method must be implemented to set the channel 
+    /// It is the only one that does not have a default implementation
+    fn channel(&self) -> String;
+    /// This method will often be implemented to set the configuration, but should work 'out of the box'
+    /// if your NSQ setup has the conventional hosts/ports
+    fn config_source(&self) -> tokio_nsq::NSQConsumerConfigSources {
+        tokio_nsq::NSQConsumerConfigSources::Daemons(vec!["127.0.0.1:4150".to_string()])
+    }
+    /// For most use cases, this defaul implementation would likely not be overwritten 
+    fn consumer(&self) -> tokio_nsq::NSQConsumer {
+        let topic = tokio_nsq::NSQTopic::new(<T as EventNSQ>::topic()).unwrap();
+        let channel = tokio_nsq::NSQChannel::new(&self.channel()).unwrap();
+        let config_source = self.config_source();
+        let config = tokio_nsq::NSQConsumerConfig::new(topic, channel)
+            // the consumer only lets you get one message at a time
+            // why then have the max_in_flight parameter? It just sends everything to in-flight, only to be re-queued
+            // just leaving it at 1 is slow but works 
+            .set_max_in_flight(1)
+            .set_sources(config_source);
+        config.build()
+    }
+    fn deserialize_event(&self, message: &tokio_nsq::NSQMessage) -> Result<T, GenericError> {
+        let event: T = serde_json::from_slice(&message.body)?;
+        Ok(event)
+    }
+}
 
-async fn post_msg<T: Serialize>(daemon: &Daemon, topic: &str, body: &T) -> Result<(), GenericError> {
-    let url = format!("{}/pub?topic={}", &daemon.host, topic);
+pub async fn post_json<T: Serialize>(host: &str, topic: &str, body: &T) -> Result<(), GenericError> {
+    let url = format!("{}/pub?topic={}", &host, topic);
     let _x: () = hyperactive::client::post_noback(&url, &body, None).await?;
     Ok(())
 }
+
+
+pub async fn post_event<T: EventNSQ>(host: &str, event: &T) -> Result<(), GenericError> {
+    let topic = <T as EventNSQ>::topic();
+    post_json(host, topic, event).await
+}
+
+pub async fn post_to<T: EventNSQ>(event: &T, daemon: &DaemonNSQ) -> Result<(), GenericError> {
+    post_event(&daemon.host, event).await
+}
+
